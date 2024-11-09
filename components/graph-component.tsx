4770c073597f202dd263db0e08ade07c80cb6e86
Alexandre Beck <alexandrebeck2003@gmail.com>
2024-11-09 15:09:38 -0300

    fix: search bugfix

--- components/graph-component.tsx
+++ components/graph-component.tsx
@@ -10,11 +10,16 @@ const ForceGraph2D = dynamic(() => import("react-force-graph-2d"), {
 const GraphComponent = () => {
   const { path } = useGraph();
 
+  const graphData = {
+    nodes: path.nodes.map((node) => ({ ...node })),
+    links: path.links.map((link) => ({ ...link })),
+  };
+
   return (
     <div className="flex w-full justify-center">
       <div className="w-2/3 flex justify-center items-center">
         <ForceGraph2D
-          graphData={path}
+          graphData={graphData}
           nodeLabel={(node) => `${node.id} (${node.genre})`}
           nodeAutoColorBy="genre"
           linkColor={() => "gray"}
